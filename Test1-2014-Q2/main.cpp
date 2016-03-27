2ac82050896de9d50a60bf46af1fafd88bc80453
randalx2 <pmisthry@gmail.com>
2016-03-27 19:39:34 +0200

    completed Q2

--- Test1-2014-Q2/main.cpp
+++ Test1-2014-Q2/main.cpp
@@ -6,9 +6,26 @@
 
 using namespace std;
 
+template <class T>  //Creating a user defined GENERIC DATATYPE T
+void print(T arr1[], const int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+	 cout << arr1[i] << "\t";
+	}
+	 cout << endl;	
+}
+
 int main()
 {
+	int a1[5] = { 1, 2, 3, 4, 5 };
+	char a2[5] = { 'a', 'b', 'c', 'd', 'e' };
+
+	//printing first integer array using template function overloading
+	print(a1, 5);
+	print(a2, 5);
 
 	system("PAUSE");
 	return 0;
 }
+
